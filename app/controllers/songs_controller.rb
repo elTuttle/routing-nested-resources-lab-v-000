579724745263f915a5128e74e9ba11a1cf4f2d00
John Tuttle <thejwt@gmail.com>
2017-11-28 04:51:37 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,7 +2,7 @@ class SongsController < ApplicationController
   def index
     if params[:artist_id]
       binding.pry
-      if Artist.find(params[:artist_id]).songs
+      if Artist.find(params[:artist_id])
       else
         flash[:alert] = "Couldn't find Artist."
         redirect_to artists_path
